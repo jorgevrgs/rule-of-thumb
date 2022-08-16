95a152e2234d61231d2b0b3b518c4b522f67c7c0
Jorge Vargas <jorge.vargas@zemoga.com>
2022-08-16 09:15:52 -0500

    feat: add banner components

---
+++ components/banner-bottom.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+export default function BannerBottom() {
+  return (
+    <aside
+      className="banner banner-bottom"
+      role="doc-tip"
+      aria-label="Submit a name"
+    >
+      <Image
+        layout="fill"
+        className="banner__background"
+        src="/assets/img/bg-people.png"
+        alt=""
+        role="none"
+      />
+      <div className="banner__left">
+        <h2 className="banner__heading">
+          Is there anyone else you would want us to add?
+        </h2>
+      </div>
+      <div className="banner__right">
+        <button className="banner__cta">Submit a name</button>
+      </div>
+    </aside>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,16 @@
 import type { NextPage } from 'next';
-import Image from 'next/image';
+import BannerBottom from '../components/banner-bottom';
+import BannerTop from '../components/banner-top';
 import Layout from '../components/layout';
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <aside
-        className="banner banner-top"
-        role="doc-tip"
-        aria-label="Speak Out"
-      >
-        <div className="banner__left">
-          <span className="banner__hairline">Speak out. Be heard.</span>
-          <span className="banner__title">Be counted</span>
-        </div>
-        <div className="banner__right">
-          <p className="banner__text">
-            Rule of Thumb is a crowd sourced court of public opinion where
-            anyone and everyone can speak out and speak freely. It’s easy: You
-            share your opinion, we analyze and put the data in a public report.
-          </p>
-        </div>
-        <button className="icon-button" aria-label="close">
-          <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
-            <g stroke="#000" strokeWidth="2" fill="none" fillRule="evenodd">
-              <path d="M1 19L19 1M1 1l18 18" />
-            </g>
-          </svg>
-        </button>
-      </aside>
+      <BannerTop />
 
       <main role="main">👉 Your code goes here 👈</main>
 
-      <aside
-        className="banner banner-bottom"
-        role="doc-tip"
-        aria-label="Submit a name"
-      >
-        <Image
-          sizes="(min-width: 750px) 1440px, 100vw"
-          className="banner__background"
-          src="/assets/img/bg-people.png"
-          alt=""
-          width={100}
-          height={100}
-          role="none"
-        />
-        <div className="banner__left">
-          <h2 className="banner__heading">
-            Is there anyone else you would want us to add?
-          </h2>
-        </div>
-        <div className="banner__right">
-          <button className="banner__cta">Submit a name</button>
-        </div>
-      </aside>
+      <BannerBottom />
     </Layout>
   );
 };
