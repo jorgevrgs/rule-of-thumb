8aefd5d8f63a20aed897a83ba6671d2dd219dfa7
Jorge Vargas <jorge.vargas@zemoga.com>
2022-08-16 09:09:37 -0500

    style: use file proper export name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default App;
