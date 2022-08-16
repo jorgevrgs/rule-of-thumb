0a2c0823eefe245ea94d7b973c41e57417bc641e
Jorge Vargas <jorge.vargas@zemoga.com>
2022-08-16 09:08:01 -0500

    style: add styles folder to included paths

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [join(__dirname, 'styles')],
+  },
+};
+
+export default nextConfig;
