4f53f7fa159c535284090a995548dcac976df7bd
Jorge Vargas <jorge.vargas@zemoga.com>
2022-08-19 09:11:26 -0500

    refactor: use tailwind for celebrity component

--- components/celebrities.tsx
+++ components/celebrities.tsx
@@ -4,7 +4,7 @@ import { Celebrity } from './celebrity';
 export default function Celebrities({ celebrities }: CelebritiesProps) {
   return (
     <>
-      <section className="grid grid-rows-fr grid-flow-col gap-4 py-4 overflow-auto">
+      <section className="grid grid-flow-col gap-4 overflow-x-auto">
         {celebrities.map((celebrity) => (
           <Celebrity celebrity={celebrity} key={celebrity.celebrityId} />
         ))}
--- components/celebrity.tsx
+++ components/celebrity.tsx
@@ -5,94 +5,26 @@ import Veredict from './veredict';
 export function Celebrity({ celebrity }: CelebrityProps) {
   return (
     <>
-      <article className="celebrity">
+      <article className="flex flex-col items-center px-4 w-[300px] h-[300px] text-white relative">
         <Image
           src={celebrity.picture}
           alt={celebrity.name}
-          className="celebrity__image"
+          className="absolute inset-0 object-cover w-full h-full"
           width={256}
           height={114}
           layout="fill"
           objectFit="cover"
         />
 
-        <div className="celebrity__details">
-          <h2 className="celebrity__name">{celebrity.name}</h2>
-          <p className="celebrity__description">{celebrity.description}</p>
+        <div className="z-10 p-8">
+          <h2 className="text-xl font-bold text-white">{celebrity.name}</h2>
+          <p className="text-lg line-clamp-3">{celebrity.description}</p>
         </div>
 
-        <div className="celebrity__stats">
+        <div className="absolute inset-x-0 bottom-0">
           <Veredict {...celebrity.votes} />
         </div>
       </article>
-      <style jsx>{`
-        .celebrity {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          padding: 0 1rem;
-          position: relative;
-          width: 256px;
-          height: 256px;
-          color: var(--color-white);
-        }
-
-        .celebrity__image {
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          position: absolute;
-        }
-
-        .celebrity__details {
-          z-index: 10;
-        }
-
-        .celebrity__name {
-          font-size: 1.5rem;
-          font-weight: 700;
-          margin: 1rem 0;
-          color: var(--color-white);
-        }
-
-        .celebrity__description {
-          font-size: 1.125rem;
-          font-weight: 400;
-          line-height: 1.5;
-          text-align: center;
-          text-overflow: ellipsis;
-          overflow: hidden;
-          height: 3rem;
-        }
-
-        .celebrity__stats {
-          width: 100%;
-          margin-top: 1rem;
-          z-index: 10;
-          position: absolute;
-          bottom: 0;
-          right: 0;
-        }
-
-        @media (min-width: 768px) {
-          .celebrity {
-            padding: 0 2rem;
-          }
-
-          .celebrity__name {
-            font-size: 2rem;
-          }
-
-          .celebrity__description {
-            font-size: 1.25rem;
-          }
-
-          .celebrity__image {
-            margin-bottom: 2rem;
-          }
-        }
-      `}</style>
     </>
   );
 }
