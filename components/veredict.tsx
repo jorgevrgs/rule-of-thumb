5f666631684d00a1fc66c879d1eb2b0c950a6286
Jorge Vargas <jorge.vargas@zemoga.com>
2022-08-19 13:15:34 -0500

    feat: add vote percentage component

--- components/veredict.tsx
+++ components/veredict.tsx
@@ -1,5 +1,5 @@
 import type { VeredictProps } from '../types';
-import Icon from './icon';
+import VotePercentage from './vote-percentage';
 
 export default function Veredict({ positive, negative }: VeredictProps) {
   const positivePercentage = (positive / (positive + negative)) * 100;
@@ -8,26 +8,8 @@ export default function Veredict({ positive, negative }: VeredictProps) {
   return (
     <>
       <div className="flex justify-between align-middle w-full">
-        <div
-          className={`flex align-middle text-lg font-bold h-12 py-2 px-4 text-white bg-green-positive/80 w-full`}
-          style={{ maxWidth: `${positivePercentage.toFixed(0)}%` }}
-          aria-label="thumbs up"
-        >
-          <Icon name="thumbs-up" width={16} height={16} />
-          <span className="ml-2 text-base font-normal">
-            {positivePercentage.toFixed(2)} %
-          </span>
-        </div>
-        <div
-          className={`flex align-middle text-lg font-bold h-12 py-2 px-4 text-white bg-yellow-negative/80 w-full`}
-          style={{ maxWidth: `${negativePercentage.toFixed(0)}%` }}
-          aria-label="thumbs down"
-        >
-          <Icon name="thumbs-down" width={16} height={16} />
-          <span className="ml-2 text-base font-normal">
-            {negativePercentage.toFixed(2)} %
-          </span>
-        </div>
+        <VotePercentage positive={positivePercentage} />
+        <VotePercentage negative={negativePercentage} />
       </div>
     </>
   );
---
+++ components/vote-percentage.tsx
@@ -0,0 +1,26 @@
+import Icon from './icon';
+
+export default function VotePercentage({
+  positive = 0,
+  negative = 0,
+}: {
+  positive?: number;
+  negative?: number;
+}) {
+  const value = positive - negative;
+  const isPositive = value > 0;
+  const ariaLabel = isPositive ? 'thumbs up' : 'thumbs down';
+  const bgColor = isPositive ? 'bg-green-positive' : 'bg-yellow-negative';
+  const iconName = isPositive ? 'thumbs-up' : 'thumbs-down';
+
+  return (
+    <div
+      className={`flex align-middle text-lg font-bold h-12 py-2 px-4 text-white ${bgColor}/80 w-full`}
+      style={{ maxWidth: `${value.toFixed(0)}%`, minWidth: '30%' }}
+      aria-label={ariaLabel}
+    >
+      <Icon name={iconName} width={16} height={16} />
+      <span className="ml-2 text-base font-xs">{value.toFixed(2)} %</span>
+    </div>
+  );
+}
