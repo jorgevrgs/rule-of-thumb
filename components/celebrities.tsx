5dc9bdece398fe18f1044ccc5522bd26f246973e
Jorge Vargas <jorge.vargas@zemoga.com>
2022-08-19 08:31:59 -0500

    refactor: use tailwind for celebrities

--- components/celebrities.tsx
+++ components/celebrities.tsx
@@ -4,22 +4,11 @@ import { Celebrity } from './celebrity';
 export default function Celebrities({ celebrities }: CelebritiesProps) {
   return (
     <>
-      <section className="celebrities">
+      <section className="grid grid-rows-fr grid-flow-col gap-4 py-4 overflow-auto">
         {celebrities.map((celebrity) => (
           <Celebrity celebrity={celebrity} key={celebrity.celebrityId} />
         ))}
       </section>
-      <style jsx>{`
-        .celebrities {
-          display: grid;
-          overflow: auto;
-          grid-template-columns: repeat(${celebrities.length}, 1fr);
-          grid-auto-rows: 1fr;
-          grid-auto-flow: column;
-          grid-gap: 1rem;
-          padding: 1rem 0;
-        }
-      `}</style>
     </>
   );
 }
