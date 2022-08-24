74880abdd450875f7e7b423bc0d02502bdb3401f
Jorge Vargas <jorge.vargas@zemoga.com>
2022-08-24 15:53:03 -0500

    feat: add last updated time ago

--- components/celebrity.tsx
+++ components/celebrity.tsx
@@ -1,8 +1,14 @@
 import Veredict from 'components/veredict';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import Image from 'next/image';
-import { CelebrityProps } from 'types';
+import type { CelebrityProps } from 'types';
 
 export function Celebrity({ celebrity }: CelebrityProps) {
+  dayjs.extend(relativeTime);
+
+  const timeAgo = dayjs().to(dayjs(celebrity.lastUpdated));
+
   return (
     <>
       <article className="flex flex-col items-center px-4 w-[300px] h-[300px] text-white relative">
@@ -15,8 +21,13 @@ export function Celebrity({ celebrity }: CelebrityProps) {
         />
 
         <div className="z-10 p-8">
-          <h2 className="text-xl font-bold text-white">{celebrity.name}</h2>
+          <h2 className="text-xl font-bold text-white line-clamp-1">
+            {celebrity.name}
+          </h2>
           <p className="text-lg line-clamp-3">{celebrity.description}</p>
+          <p className="text-right text-sm mt-4 w-full">
+            {`${timeAgo} in ${celebrity.category}`}
+          </p>
         </div>
 
         <div className="absolute inset-x-0 bottom-0">
