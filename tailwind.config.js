0fdd5c9b0829bbc9c7edcfff3b5da4efc9e38f6e
Jorge Vargas <jorge.vargas@zemoga.com>
2022-08-24 12:45:11 -0500

    chore: remove unused colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,6 @@ module.exports = {
         yellow: {
           negative: 'rgb(249, 173, 29)',
         },
-        'dark-background': 'rgba(0, 0, 0, 0.4)',
-        'darker-background': 'rgba(0, 0, 0, 0.6)',
-        'darker-gray': 'rgba(51, 51, 51, 1)',
-        'dark-gray': 'rgba(70, 70, 70, 1)',
-        'light-gray': 'rgba(235, 235, 235, 1)',
-        'light-background': 'rgba(255, 255, 255, 0.4)',
-        'lighter-background': 'rgba(255, 255, 255, 0.8)',
       },
     },
   },
