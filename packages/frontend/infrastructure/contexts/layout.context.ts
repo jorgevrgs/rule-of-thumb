46e553a1cc50162ba6f8eba8a5baa2409adec19b
Jorge Vargas <jorge.vargas@zemoga.com>
2022-09-05 16:22:13 -0500

    fix: missing default context value

--- packages/frontend/infrastructure/contexts/layout.context.ts
+++ packages/frontend/infrastructure/contexts/layout.context.ts
@@ -1,7 +1,9 @@
 import { createContext } from 'react';
+import { DeviceType } from '../../domain';
 import type { LayoutContextType } from '../../domain/types';
 
 export const LayoutContext = createContext<LayoutContextType>({
   navLinks: [],
   celebrity: undefined,
+  deviceType: DeviceType.mobile,
 });
