3df4383062a2311d337d912eabd7e7563d7c06a5
Jorge Vargas <jorge.vargas@zemoga.com>
2022-09-08 14:46:24 -0500

    style: use ui component

--- packages/frontend/infrastructure/features/components/vote-percentage.tsx
+++ packages/frontend/infrastructure/features/components/vote-percentage.tsx
@@ -2,21 +2,21 @@ import classNames from 'classnames/bind';
 import { roundValue } from '../../../application/utils';
 import { Icon } from '../../components';
 
-export default function VotePercentage({
-  positive = 0,
-  negative = 0,
-}: {
-  positive?: number;
-  negative?: number;
-}) {
-  const value = positive - negative;
-  const isPositive = value > 0;
-  const ariaLabel = isPositive ? 'thumbs up' : 'thumbs down';
-  const bgColor = isPositive ? 'bg-green-positive' : 'bg-yellow-negative';
-  const iconName = isPositive ? 'thumbs-up' : 'thumbs-down';
-  const absValue = roundValue(Math.abs(value), 2);
-  const roundedValue = roundValue(absValue);
+interface CelebrityDesktopUIProps {
+  bgColor: string;
+  ariaLabel: string;
+  roundedValue: number;
+  absValue: number;
+  iconName: string;
+}
 
+export function VotePercentageUI({
+  bgColor,
+  ariaLabel,
+  roundedValue,
+  absValue,
+  iconName,
+}: CelebrityDesktopUIProps) {
   return (
     <div
       className={classNames([
@@ -37,5 +37,31 @@ export default function VotePercentage({
       <Icon name={iconName} width={16} height={16} />
       <span className="ml-2 text-base font-xs">{absValue} %</span>
     </div>
+  );
+}
+
+export default function VotePercentage({
+  positive = 0,
+  negative = 0,
+}: {
+  positive?: number;
+  negative?: number;
+}) {
+  const value = positive - negative;
+  const isPositive = value > 0;
+  const ariaLabel = isPositive ? 'thumbs up' : 'thumbs down';
+  const bgColor = isPositive ? 'bg-green-positive' : 'bg-yellow-negative';
+  const iconName = isPositive ? 'thumbs-up' : 'thumbs-down';
+  const absValue = roundValue(Math.abs(value), 2);
+  const roundedValue = roundValue(absValue);
+
+  return (
+    <VotePercentageUI
+      bgColor={bgColor}
+      ariaLabel={ariaLabel}
+      roundedValue={roundedValue}
+      absValue={absValue}
+      iconName={iconName}
+    />
   );
 }
