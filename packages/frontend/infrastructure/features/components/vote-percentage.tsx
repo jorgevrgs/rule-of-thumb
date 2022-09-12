35119d33e597f89b627a112575fb1dd431b662af
Jorge Vargas <jorge.vargas@zemoga.com>
2022-09-12 10:01:30 -0500

    fix: class name bind error

--- packages/frontend/infrastructure/features/components/vote-percentage.tsx
+++ packages/frontend/infrastructure/features/components/vote-percentage.tsx
@@ -1,44 +1,5 @@
-import classNames from 'classnames/bind';
 import { roundValue } from '../../../application/utils';
 import { Icon } from '../../components';
-
-interface CelebrityDesktopUIProps {
-  bgColor: string;
-  ariaLabel: string;
-  roundedValue: number;
-  absValue: number;
-  iconName: string;
-}
-
-export function VotePercentageUI({
-  bgColor,
-  ariaLabel,
-  roundedValue,
-  absValue,
-  iconName,
-}: CelebrityDesktopUIProps) {
-  return (
-    <div
-      className={classNames([
-        'flex',
-        'items-center',
-        'justify-center',
-        'text-lg',
-        'font-bold',
-        'h-12',
-        'py-2',
-        'px-4',
-        'text-white',
-        `${bgColor}/80`,
-      ])}
-      aria-label={ariaLabel}
-      style={{ width: `${roundedValue}%`, minWidth: '30%' }}
-    >
-      <Icon name={iconName} width={16} height={16} />
-      <span className="ml-2 text-base font-xs">{`${absValue} %`}</span>
-    </div>
-  );
-}
 
 export default function VotePercentage({
   positive = 0,
@@ -56,12 +17,13 @@ export default function VotePercentage({
   const roundedValue = roundValue(absValue);
 
   return (
-    <VotePercentageUI
-      bgColor={bgColor}
-      ariaLabel={ariaLabel}
-      roundedValue={roundedValue}
-      absValue={absValue}
-      iconName={iconName}
-    />
+    <div
+      className={`flex items-center justify-center text-lg font-bold h-12 py-2 px-4 text-white ${bgColor}/80`}
+      aria-label={ariaLabel}
+      style={{ width: `${roundedValue}%`, minWidth: '30%' }}
+    >
+      <Icon name={iconName} width={16} height={16} />
+      <span className="ml-2 text-base font-xs">{`${absValue} %`}</span>
+    </div>
   );
 }
