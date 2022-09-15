b5aa8711852b5bdb39fedb071139bb4a909c7e20
Jorge Vargas <jorge.vargas@zemoga.com>
2022-09-15 12:26:26 -0500

    fix: update votes after voting

--- packages/frontend/infrastructure/features/components/vote.tsx
+++ packages/frontend/infrastructure/features/components/vote.tsx
@@ -1,7 +1,8 @@
-import { useMemo } from 'react';
+import { useEffect } from 'react';
 import type { UpdateVoteParams, VoteProps } from '../../../domain/types';
+import { useAppDispatch } from '../../hooks';
 import { useGetCelebrityById } from '../hooks';
-import { useUpdateVoteMutation } from '../slices';
+import { celebritiesSlice, useUpdateVoteMutation } from '../slices';
 import Veredict from './veredict';
 import { VoteAgain } from './vote-again';
 import { VoteNow } from './vote-now';
@@ -9,25 +10,44 @@ import VoteResult from './vote-result';
 
 export default function Vote({ celebrityId }: VoteProps) {
   const [mutate, result] = useUpdateVoteMutation();
-  const { reset, isSuccess, isLoading, data } = result;
+  const { reset, isSuccess, isLoading, data: updatedCelebrity } = result;
   const { data: celebrity } = useGetCelebrityById(celebrityId);
-
-  const votes = useMemo(() => {
-    const current = data || celebrity;
-
-    return current?.votes;
-  }, [celebrity, data]);
+  const dispatch = useAppDispatch();
 
   const updateVote = ({ celebrityId, vote }: UpdateVoteParams) => {
     mutate({ celebrityId, vote });
   };
 
+  useEffect(() => {
+    console.log('Detecting changes in the vote result', updatedCelebrity);
+    if (updatedCelebrity) {
+      dispatch(
+        celebritiesSlice.util.updateQueryData(
+          'getCelebrities',
+          undefined,
+          (celebrities) => {
+            const index = celebrities.findIndex(
+              (celebrity) => celebrity.celebrityId === celebrityId
+            );
+
+            if (index > -1 && updatedCelebrity) {
+              const celebrity = celebrities[index];
+              celebrity.votes = updatedCelebrity.votes;
+            }
+
+            return celebrities;
+          }
+        )
+      );
+    }
+  }, [updatedCelebrity, celebrityId, dispatch]);
+
   return (
     <>
-      {votes && <VoteResult {...votes} />}
+      {celebrity && <VoteResult {...celebrity.votes} />}
 
       <div className="absolute inset-x-0 bottom-0">
-        {votes && <Veredict {...votes} />}
+        {celebrity && <Veredict {...celebrity.votes} />}
       </div>
 
       {isSuccess ? (
