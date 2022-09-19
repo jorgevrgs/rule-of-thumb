c8003541c6bf77a7002f547212f731049d345897
Jorge Vargas <jorge.vargas@zemoga.com>
2022-09-19 08:42:01 -0500

    style: add skip to main content

--- packages/frontend/infrastructure/components/navbar.tsx
+++ packages/frontend/infrastructure/components/navbar.tsx
@@ -9,6 +9,13 @@ function Navbar() {
 
   return (
     <nav className="nav w-full z-30" role="navigation">
+      <a
+        href="#main"
+        className="skip-link absolute -top-full w-px h-px overflow-hidden mx-auto transition-position delay-300 ease-in-out focus:w-auto focus:h-auto focus:top-0 focus:left-1/2 focus:-translate-x-1/2  focus:z-50 focus:bg-black/90 focus:text-white focus:px-4 focus:py-2 focus:rounded-b-lg"
+      >
+        Skip to main content
+      </a>
+
       <div className="flex justify-between w-full md:relative md:w-full md:max-w-7xl md:mx-auto">
         <h1 className="nav__logo">Rule of thumb.</h1>
         <button className="nav__hamburger icon-button">
