4b36a690c8fc302b7dc16d5cb42b4dd5f81bec2f
Jorge Vargas <jorge.vargas@zemoga.com>
2022-09-20 09:55:57 -0500

    fix: catch errors

--- packages/backend/application/utils/error.util.ts
+++ packages/backend/application/utils/error.util.ts
@@ -1,15 +1,16 @@
+import { logger } from '@app/shared';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export function tryCatchAsync<T>(
   cb: (req: NextApiRequest, res: NextApiResponse<T>) => Promise<void>
 ) {
   return (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-      cb(req, res);
-    } catch (error) {
+    cb(req, res).catch((error) => {
+      logger.error(error);
+
       res.status(400).json({
         error: error instanceof Error ? error.message : 'Bad Request',
       });
-    }
+    });
   };
 }
